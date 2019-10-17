c78fad7ebe96b7421cfb05dc2543f6a77c482e6a
christocarr <christocarr@outlook.com>
2019-10-17 11:57:22 +0100

    Add form heading

--- src/components/PostcodeSearch.js
+++ src/components/PostcodeSearch.js
@@ -10,39 +10,42 @@ const PostcodeSearch = ({
   distanceSelect
 }) => {
   return (
-    <form onSubmit={postcodeSearch} className="postcode-search">
-      <label htmlFor="postcodeInput">
-        Postcode:
-        <input
-          type="text"
-          id="postcodeInput"
-          value={postcode}
-          onChange={postcodeChange}
-        />
-      </label>
-      <div>
-        <label htmlFor="twoKilometres">
-          2km:
+    <form onSubmit={postcodeSearch}>
+      <p>Search for activities within a range</p>
+      <div className="postcode-search">
+        <label htmlFor="postcodeInput">
+          Postcode:
           <input
-            type="radio"
-            id="twoKilometres"
-            value="option2km"
-            checked={selectedDistance === 2000}
-            onChange={distanceSelect}
+            type="text"
+            id="postcodeInput"
+            value={postcode}
+            onChange={postcodeChange}
           />
         </label>
-        <label htmlFor="fiveKilometres">
-          5km:
-          <input
-            type="radio"
-            id="fiveKilometres"
-            value="option5km"
-            checked={selectedDistance === 5000}
-            onChange={distanceSelect}
-          />
-        </label>
+        <div>
+          <label htmlFor="twoKilometres">
+            2km:
+            <input
+              type="radio"
+              id="twoKilometres"
+              value="option2km"
+              checked={selectedDistance === 2000}
+              onChange={distanceSelect}
+            />
+          </label>
+          <label htmlFor="fiveKilometres">
+            5km:
+            <input
+              type="radio"
+              id="fiveKilometres"
+              value="option5km"
+              checked={selectedDistance === 5000}
+              onChange={distanceSelect}
+            />
+          </label>
+        </div>
+        <input type="submit" value="Submit" />
       </div>
-      <input type="submit" value="Submit" />
     </form>
   );
 };
