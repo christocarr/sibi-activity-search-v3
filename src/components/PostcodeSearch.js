1f9133f68e033e3b7089b1cbe40f603fda89e7fa
christocarr <christocarr@outlook.com>
2019-10-17 11:17:40 +0100

    Modify layout

--- src/components/PostcodeSearch.js
+++ src/components/PostcodeSearch.js
@@ -1,39 +1,50 @@
-import React from 'react'
+import React from "react";
 
-const PostcodeSearch = ({ postcodeChange, postcodeSearch, postcode, latitude, longitude, selectedDistance, distanceSelect }) => {
+const PostcodeSearch = ({
+  postcodeChange,
+  postcodeSearch,
+  postcode,
+  latitude,
+  longitude,
+  selectedDistance,
+  distanceSelect
+}) => {
   return (
-    <form onSubmit={postcodeSearch} >
-      <label htmlFor="postcodeInput">Postcode: 
-        <input 
-          type="text" 
-          id="postcodeInput" 
-          value={postcode} 
-          onChange={postcodeChange} 
+    <form onSubmit={postcodeSearch} className="postcode-search">
+      <label htmlFor="postcodeInput">
+        Postcode:
+        <input
+          type="text"
+          id="postcodeInput"
+          value={postcode}
+          onChange={postcodeChange}
         />
       </label>
-      <label htmlFor="twoKilometres">2km
-        <input 
-          type="radio" 
-          id="twoKilometres"
-          value='option2km'
-          checked={selectedDistance === 2000}
-          onChange={distanceSelect}
-        />
-      </label>
-      <label htmlFor="fiveKilometres">5km
-        <input 
-          type="radio" 
-          id="fiveKilometres"
-          value='option5km'
-          checked={selectedDistance === 5000}
-          onChange={distanceSelect}
-        />
-      </label>
+      <div>
+        <label htmlFor="twoKilometres">
+          2km:
+          <input
+            type="radio"
+            id="twoKilometres"
+            value="option2km"
+            checked={selectedDistance === 2000}
+            onChange={distanceSelect}
+          />
+        </label>
+        <label htmlFor="fiveKilometres">
+          5km:
+          <input
+            type="radio"
+            id="fiveKilometres"
+            value="option5km"
+            checked={selectedDistance === 5000}
+            onChange={distanceSelect}
+          />
+        </label>
+      </div>
       <input type="submit" value="Submit" />
-      <p>latitude: {latitude}</p>
-      <p>longitude: {longitude}</p>
     </form>
-  )
-}
+  );
+};
 
-export default PostcodeSearch
+export default PostcodeSearch;
