a2cefb6b74b849308dcedeba98ebcdfaae06f12d
Chris Carr <christocarr@outlook.com>
2019-10-19 16:35:47 +0100

    Add loading spinner component

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import ReactLoading from 'react-loading'
 
 const Loading = () => {
   return (
-    <p>Loading...</p>
+    <div className="loading-container">
+      <ReactLoading type="spinningBubbles"  color='blue' height='10%' width='10%' />
+    </div>
+    
   )
 }
 
