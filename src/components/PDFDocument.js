c4d35fdddb7011dc3225d018f98651c9b0e784fc
Chris Carr <christocarr@outlook.com>
2019-10-20 18:56:32 +0100

    Add activity detail

--- src/components/PDFDocument.js
+++ src/components/PDFDocument.js
@@ -26,7 +26,6 @@ const styles = StyleSheet.create({
 })
 
 const PDFDocument = ({ activityTypes, activities }) => {
-  // const { NameOfService, AccessibilityDetails, AddressLine1, AddressLine2, AddressLine3, Postcode, OtherDetailedInformation, MondayStart, MondayEnd, TuesdayStart, TuesdayEnd, WednesdayStart, WednesdayEnd, ThursdayStart, ThursdayEnd, FridayStart, FridayEnd, SaturdayStart, SaturdayEnd, SundayStart, SundayEnd, Cost,  Buses, TubeAndTrains, CarParkingDetails, Name1, PhoneNumber1, Email1, Name2, PhoneNumber2, Email2, Website, OtherContactInfo  } = activities
   
   const types = activityTypes.map((type, index) => {
     if (activityTypes.length > 1) {
@@ -37,6 +36,56 @@ const PDFDocument = ({ activityTypes, activities }) => {
     )
   })
   
+  const pdfActivities = activities.map((activity, index) => {
+    return (
+      <View key={index}>
+        <View style={styles.heading}>
+          <Text>{activity.NameOfService}</Text>
+        </View>
+        <View style={styles.text}>
+          <Text>{activity.AddressLine1}</Text>
+          <Text>{activity.AddressLine2}</Text>
+          <Text>{activity.AddressLine3}</Text>
+          <Text>{activity.Postcode}</Text>
+        </View>
+        <View style={styles.text}>
+          <Text>{activity.OtherDetailedInformation}</Text>
+        </View>
+        <View style={styles.text}>
+          {activity.MondayStart ? <Text>Monday: {activity.MondayStart} - {activity.MondayEnd}</Text> : null}
+          {activity.TuesdayStart ? <Text>Tuesday: {activity.TuesdayStart} - {activity.TuesdayEnd}</Text> : null}
+          {activity.WednesdayStart ? <Text>Wednesday: {activity.WednesdayStart} - {activity.WednesdayEnd}</Text> : null}
+          {activity.ThursdayStart ? <Text>Thursday: {activity.ThursdayStart} - {activity.ThursdayEnd}</Text> : null}
+          {activity.FridayStart ? <Text>Friday: {activity.FridayStart} - {activity.FridayEnd}</Text> : null}
+          {activity.SaturdayStart ? <Text>Saturday: {activity.SaturdayStart} - {activity.SaturdayEnd}</Text> : null}
+          {activity.SundayStart ? <Text>Sunday: {activity.SundayStart} - {activity.SundayEnd}</Text> : null}
+        </View>
+        <View style={styles.text}>
+          <Text>Cost: {activity.Cost}</Text>
+        </View>
+        <View style={styles.text}>
+          <Text>Transport:</Text>
+          <Text>Buses: {activity.Buses}</Text>
+          <Text>Tube and Trains: {activity.TubeAndTrains}</Text>
+          <Text>Car park Availibility: {activity.CarParkingDetails}</Text>
+        </View>
+        <View style={styles.text}>
+          {activity.AccessibilityDetails ? <Text>Accessibility: {activity.AccessibilityDetails}</Text> : null}
+        </View>
+        <View style={styles.text}>
+          <Text>Contact Details:</Text>
+          <Text>{activity.Name1}: {activity.PhoneNumber1}; {activity.Email1}</Text>
+          {activity.Name2 ? <Text>{activity.Name2}: {activity.PhoneNumber2}; {activity.Email2}</Text> : null}
+        </View>
+        <View style={styles.text}>
+          {activity.Website ? <Text>{activity.Website}</Text> : null}
+        </View>
+        <View style={styles.text}>
+          {activity.OtherContactInfo ? <Text>{activity.OtherContactInfo}</Text> : null}
+        </View>
+      </View>
+    )
+  })
     
   return (
     <PDFViewer>
@@ -49,51 +98,12 @@ const PDFDocument = ({ activityTypes, activities }) => {
             <Text style={styles.standardText}>Going to a new group can feel daunting but be very worthwhile. Groups all vary so you may need to try a few groups or go more than once to get to know people and find somewhere that suits you. And while we provide information about what groups are out there we can't guarantee them.</Text>
             <Text style={styles.standardText}>You can simply turn up to most groups, they run on a drop-on basis and there's no need to book unless it says so. While our information is fairly recent it's best to double check before making a long journey as classes can stop or take a break.</Text>
           </View>
-
-          {/* <View style={styles.heading}>
-            <Text>{NameOfService}</Text>
+          {pdfActivities}
+          <View>
+            <Text render={({ pageNumber, totalPages }) => (
+              `page ${pageNumber} of ${totalPages}`
+            )} fixed />
           </View>
-          <View style={styles.text}>
-            <Text>{AddressLine1}</Text>
-            <Text>{AddressLine2}</Text>
-            <Text>{AddressLine3}</Text>
-            <Text>{Postcode}</Text>
-          </View>
-          <View style={styles.text}>
-            <Text>{OtherDetailedInformation}</Text>
-          </View>
-          <View style={styles.text}>
-            {MondayStart ? <Text>Monday: {MondayStart} - {MondayEnd}</Text> : null}
-            {TuesdayStart ? <Text>Tuesday: {TuesdayStart} - {TuesdayEnd}</Text> : null}
-            {WednesdayStart ? <Text>Wednesday: {WednesdayStart} - {WednesdayEnd}</Text> : null}
-            {ThursdayStart ? <Text>Thursday: {ThursdayStart} - {ThursdayEnd}</Text> : null}
-            {FridayStart ? <Text>Friday: {FridayStart} - {FridayEnd}</Text> : null}
-            {SaturdayStart ? <Text>Saturday: {SaturdayStart} - {SaturdayEnd}</Text> : null}
-            {SundayStart ? <Text>Sunday: {SundayStart} - {SundayEnd}</Text> : null}
-          </View>
-          <View style={styles.text}>
-            <Text>Cost: {Cost}</Text>
-          </View>
-          <View style={styles.text}>
-            <Text>Transport:</Text>
-            <Text>Buses: {Buses}</Text>
-            <Text>Tube and Trains: {TubeAndTrains}</Text>
-            <Text>Car park Availibility: {CarParkingDetails}</Text>
-          </View>
-          <View style={styles.text}>
-            {AccessibilityDetails ? <Text>Accessibility: {AccessibilityDetails}</Text> : null}
-          </View>
-          <View style={styles.text}>
-            <Text>Contact Details:</Text>
-            <Text>{Name1}: {PhoneNumber1}; {Email1}</Text>
-            {Name2 ? <Text>{Name2}: {PhoneNumber2}; {Email2}</Text> : null}
-          </View>
-          <View style={styles.text}>
-            {Website ? <Text>{Website}</Text> : null}
-          </View>
-          <View style={styles.text}>
-            {OtherContactInfo ? <Text>{OtherContactInfo}</Text> : null}
-          </View> */}
         </Page>
       </Document>
     </PDFViewer>
