c634338f6790f89fd3f06032a2e0ce4f1d0cf9cb
Chris Carr <christocarr@outlook.com>
2019-10-24 18:08:50 +0100

    Modify nav layout

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Logo from '../images/logo-310x310.png'
 
 const Nav = () => {
   return (
     <nav>
-      <h1>Logo</h1>
-      <ul>
-        <Link to="/">Search</Link>
-        <Link to="/review">Review</Link>
-      </ul>
+      <div className="nav-items-container">
+        <img src={Logo} alt="SIBI Logo" width="90px" />
+        <ul>
+          <Link to="/">Search</Link>
+          <Link to="/review">Review</Link>
+        </ul>
+      </div>
     </nav>
   );
 };
