a996c51d51c66f47893374db83f46a71fbd6aec7
Chris Carr <christocarr@outlook.com>
2019-11-13 14:25:23 +0000

    Add active true attribute

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,7 +10,7 @@ const Nav = () => {
       <div className="nav-items-container">
         <img src={Logo} alt="SIBI Logo" width="90px" />
         <ul>
-          <NavLink to="/" activeStyle={style}>Search</NavLink>
+          <NavLink exact={true} to="/" activeStyle={style}>Search</NavLink>
           <NavLink to="/review" activeStyle={style}>Review</NavLink>
         </ul>
       </div>
