eaaedf61029a8cc067c244c583cdc08e558fa1af
Chris Carr <christocarr@outlook.com>
2019-11-13 14:22:54 +0000

    Add active link styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import Logo from '../images/logo-310x310.png'
+import { NONAME } from "dns";
 
 const Nav = () => {
+  const style = {textDecoration: 'none', color: 'black'}
   return (
     <nav>
       <div className="nav-items-container">
         <img src={Logo} alt="SIBI Logo" width="90px" />
         <ul>
-          <NavLink to="/">Search</NavLink>
-          <NavLink to="/review">Review</NavLink>
+          <NavLink to="/" activeStyle={style}>Search</NavLink>
+          <NavLink to="/review" activeStyle={style}>Review</NavLink>
         </ul>
       </div>
     </nav>
