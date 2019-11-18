7b2fed1ed6e901875a61e469fe53ce879cf42cde
Chris Carr <christocarr@outlook.com>
2019-11-18 16:29:07 +0000

    Add link to portfolio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
+      <p>Created by <a href="https://www.chriscarr.dev">Christopher Carr</a></p>
       <a href="https://www.cvsbrent.org.uk/services/social-involvement-in-brent-initiative/">About</a>
     </footer>
   )
