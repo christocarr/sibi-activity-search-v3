f53378d724fc0679ccd9c74742aee9d7bad0a22a
christocarr <christocarr@outlook.com>
2020-09-19 09:49:19 +0100

    Update link to new portfolio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-      <p>Created by <a href="https://www.chriscarr.dev">Christopher Carr</a></p>
+      <p>Created by <a href="https://www.iamchriscarr.com/">Christopher Carr</a></p>
       <p>More about the <a href="https://www.cvsbrent.org.uk/services/social-involvement-in-brent-initiative/">SIBI</a> program.</p>
     </footer>
   )
